f5b29d1c39def9bef709b7e87e4f8bc8db5db679
shonb <shonberko@gmail.com>
2024-07-20 15:44:41 +0300

    d

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+//        IntStream.range(0,Integer.MAX_VALUE).filter(Character::isEmoji)
+//                .mapToObj(Character::toChars)
+//                .map(String::valueOf)
+//           .forEach(System.out::print);
+
+         new Window();
+
+
+        };
+
+    }
+
+
+
+
+
